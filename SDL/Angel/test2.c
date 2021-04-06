dd22a772d64679a61a9cae9bffbe01346222b3f4
L2-Chess <you@example.com>
2021-04-06 23:03:34 +0200

    ajout

--- SDL/Angel/test2.c
+++ SDL/Angel/test2.c
@@ -9,10 +9,6 @@
 
 int mouse_x;
 int mouse_y;
-int IsInSquare = 0;
-int IsInSquare2 = 0;
-int IsInSquare3 = 0;
-int IsInSquare4 = 0;
 int mouse_left;
 int Action1 = 0;
 int GrabMode = 0;
@@ -20,6 +16,13 @@ int PieceDrop = 0;
 //int Action2 = 0;
 //int Action3 = 0;
 //int Action4 = 0;
+
+int IsInSquare = 0;
+
+//INIT DE TOUTES LES CASES
+
+char CASE[2];
+
 
 
 int main(int argc, char* argv[])
@@ -70,18 +73,43 @@ int main(int argc, char* argv[])
     SDL_Event events;
     float isOpen = 1;
 
-    //creation rectangle 1
+    //PIECE
     SDL_Rect square;
     square.x = 300;
     square.y = 200;
-    square.w = 200;
+    square.w = 50;
     square.h = 50;
 
+
+    // TOUTES LES CASES
+
+    //A1
+    SDL_Rect a1;
+    a1.x = 50;
+    a1.y = 600;
+    a1.w = 100;
+    a1.h = 100;
     
+    //B1
+    SDL_Rect b1;
+    b1.x = 150;
+    b1.y = 600;
+    b1.w = 100;
+    b1.h = 100;
 
-    
+    //C1
+    SDL_Rect c1;
+    c1.x = 250;
+    c1.y = 600;
+    c1.w = 100;
+    c1.h = 100;
 
-
+    //D1
+    SDL_Rect d1;
+    d1.x = 350;
+    d1.y = 600;
+    d1.w = 100;
+    d1.h = 100;
 
     while (isOpen)
     {
@@ -103,9 +131,31 @@ int main(int argc, char* argv[])
                 else {
                     IsInSquare = 0;
                 }
-                
 
-                SDL_Log("Mouse x = %i , Mouse y = %i , IIS1 = %i , GRAB = %i , PIECE_DROP = %i", mouse_x, mouse_y, IsInSquare, GrabMode, PieceDrop);
+                ///////////////////////////////
+                if ((mouse_x > a1.x && mouse_x < a1.x + a1.w) && (mouse_y > a1.y && mouse_y < a1.y + a1.h)) {
+                    CASE[0] = 'A';
+                    CASE[1] = '1';
+                }
+                else if ((mouse_x > b1.x && mouse_x < b1.x + b1.w) && (mouse_y > b1.y && mouse_y < b1.y + b1.h)) {
+                    CASE[0] = 'B';
+                    CASE[1] = '1';
+                }
+                else if ((mouse_x > c1.x && mouse_x < c1.x + c1.w) && (mouse_y > c1.y && mouse_y < c1.y + c1.h)) {
+                    CASE[0] = 'C';
+                    CASE[1] = '1';
+                }
+                else if ((mouse_x > d1.x && mouse_x < d1.x + d1.w) && (mouse_y > d1.y && mouse_y < d1.y + d1.h)) {
+                    CASE[0] = 'D';
+                    CASE[1] = '1';
+                }
+                else {
+                    CASE[0] = 'R';
+                    CASE[1] = 'R';
+                }
+                
+           
+                SDL_Log("Mouse x = %i , Mouse y = %i , IIS1 = %i , CASE = %s, GRAB = %i , PIECE_DROP = %i", mouse_x, mouse_y, IsInSquare, CASE, GrabMode, PieceDrop);
 
 
                 
@@ -132,18 +182,113 @@ int main(int argc, char* argv[])
                    
                }
                 
+               //VALEURE SAVOIR CASE DE DEPART
+
                
                break;
                 
             case SDL_MOUSEBUTTONUP:
 
-               
+                if (GrabMode == 1) {
+                    switch (CASE[0])
+                    {
+                    case 'A':
+                        switch (CASE[1])
+                        {
+                        case '1':
+                            square.x = a1.x + (a1.w / 4);
+                            square.y = a1.y + (a1.h / 4);
+                            break;
+                        case '2':
+                            break;
+                        case '3':
+                            break;
+                        case '4':
+                            break;
+                        case '5':
+                            break;
+                        case '6':
+                            break;
+                        case '7':
+                            break;
+                        case '8':
+                            break;
+                        }
+                        break;
+                    case 'B':
+                        switch (CASE[1])
+                        {
+                        case '1':
+                            square.x = b1.x + (b1.w / 4);
+                            square.y = b1.y + (b1.h / 4);
+                            break;
+                        case '2':
+                            break;
+                        case '3':
+                            break;
+                        case '4':
+                            break;
+                        case '5':
+                            break;
+                        case '6':
+                            break;
+                        case '7':
+                            break;
+                        case '8':
+                            break;
+                        }
+                        break;
+                    case 'C':
+                        switch (CASE[1])
+                        {
+                        case '1':
+                            square.x = c1.x + (c1.w / 4);
+                            square.y = c1.y + (c1.h / 4);
+                            break;
+                        case '2':
+                            break;
+                        case '3':
+                            break;
+                        case '4':
+                            break;
+                        case '5':
+                            break;
+                        case '6':
+                            break;
+                        case '7':
+                            break;
+                        case '8':
+                            break;
+                        }
+                        break;
+                    case 'D':
+                        switch (CASE[1])
+                        {
+                        case '1':
+                            square.x = d1.x + (d1.w / 4);
+                            square.y = d1.y + (d1.h / 4);
+                            break;
+                        case '2':
+                            break;
+                        case '3':
+                            break;
+                        case '4':
+                            break;
+                        case '5':
+                            break;
+                        case '6':
+                            break;
+                        case '7':
+                            break;
+                        case '8':
+                            break;
+                        }
+                        break;
+                    }
+               }
                 
 
-               if (GrabMode == 1) {
-                    square.x = mouse_x - (square.w / 2);
-                    square.y = mouse_y - (square.h / 2);
-               }
+               
 
                GrabMode = 0;
                break;
@@ -186,11 +331,30 @@ int main(int argc, char* argv[])
         //SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
         //SDL_RenderDrawPoint(pRenderer, 200, 200);
 
-        
+
+
+        //RENDER DES CASES
+        //
+        //
+        //
+        //NOIR
+        SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
+        SDL_RenderFillRect(pRenderer, &a1);
+        SDL_RenderFillRect(pRenderer, &c1);
+        //BLANC
+        SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 0);
+        SDL_RenderFillRect(pRenderer, &b1);
+        SDL_RenderFillRect(pRenderer, &d1);
+
+        //
+        //
+        //
 
         //RECTANGLE TEST
         SDL_SetRenderDrawColor(pRenderer, 0, 255, 0, 0);
         SDL_RenderFillRect(pRenderer, &square);
+
+        
 
         SDL_RenderPresent(pRenderer);
 
